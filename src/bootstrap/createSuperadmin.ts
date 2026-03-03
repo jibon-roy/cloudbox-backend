8030370ff7413c4683c0b20eb2415ed7679008e6
Jibon Krishna Roy <mailme.jibon@gmail.com>
2026-03-03 22:05:33 +0600

    fix: admin update

--- src/bootstrap/createSuperadmin.ts
+++ src/bootstrap/createSuperadmin.ts
@@ -9,6 +9,7 @@ export const initiateAdmin = async () => {
     email: "superadmin@gmail.com",
     password: "12345678",
     role: Role.ADMIN,
+    verified_at: new Date(),
   };
 
   const existingAdmin = await prisma.user.findUnique({
